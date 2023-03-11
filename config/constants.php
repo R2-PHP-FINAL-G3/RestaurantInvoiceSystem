229d49ca9516303afdc812c2a647b1d9fdfed159
kyrillos-gaber <kyrillosgaber@gmail.com>
2023-03-11 19:45:20 +0200

    change constant ip and port in config file

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'DELIVERY_SRV_API' => 'http://127.0.0.2:80/api/',
+    'DELIVERY_SRV_API' => 'http://127.0.0.1:8000/api/',
     'API_KEY' => '2|aqQghWswGXwUAZv2PaD8vGNeZeR7QO8nXLYdYTQK'
 
 ];
